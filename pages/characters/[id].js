3e1176ef328c71f378dc9dbf6089cdcc4d02f0c8
Merle Makoben <merle.makoben@neuefische.de>
2024-01-24 11:44:27 +0100

    Add Solution

---
+++ pages/characters/[id].js
@@ -0,0 +1,30 @@
+import Card from "../../components/Card";
+import Layout from "../../components/Layout";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+
+export default function Character() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const {
+    data: character,
+    error,
+    isLoading,
+  } = useSWR(`https://swapi.dev/api/people/${id}`);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  return (
+    <Layout>
+      <Card
+        id={id}
+        name={character.name}
+        height={character.height}
+        eyeColor={character.eye_color}
+        birthYear={character.birth_year}
+      />
+    </Layout>
+  );
+}
